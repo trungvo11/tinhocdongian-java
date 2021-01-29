c2818c010b0ca49742e665e0465f588de313604e
trung vo <vttrung1710@gmail.com>
2021-01-29 10:20:19 +0000

    sua helloworld

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -9,6 +9,6 @@ public class HelloWorld implements SparkApplication {
 
 	@Override
 	public void init() {
-		get("/hello", (req, res) -> "Hello Trung1234234");
+		get("/hello", (req, res) -> "Hello Trung12asdasdsadasd34234");
 	}
 }
